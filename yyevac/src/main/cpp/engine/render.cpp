e3da73f7582acd960c4bb2774d393e73e139b692
zengjiale <zengjiale2@yy.com>
2025-06-06 17:04:04 +0800

    调整blend模式

--- yyevac/src/main/cpp/engine/render.cpp
+++ yyevac/src/main/cpp/engine/render.cpp
@@ -123,12 +123,12 @@ void yyeva::Render::draw() {
         //     GLenum dstRGB,
         //     GLenum srcAlpha,
         //     GLenum dstAlpha);
-        if (blendMode == 1) {
-            glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE,
-                                GL_ONE_MINUS_SRC_ALPHA);
-        } else {
+//        if (blendMode == 1) {
+//            glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_SRC_ALPHA,
+//                                GL_ONE_MINUS_SRC_ALPHA);
+//        } else {
             glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-        }
+//        }
 //        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
         glDisable(GL_BLEND);
